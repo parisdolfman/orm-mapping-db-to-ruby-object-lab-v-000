63a94c13943c8135296bf3a1d1239bb011ca157a
Paris Dolfman <parisdolfman@gmail.com>
2020-06-25 19:26:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,14 +71,14 @@ class Student
   end
 
   def self.all_students_in_grade_X(x)
-  sql = <<-SQL 
+  sql = <<-SQL
     SELECT * FROM students WHERE grade = ?
   SQL
 
   DB[:conn].execute(sql, x).map do |row|
     self.new_from_db(row)
-    end 
-  end 
+    end
+  end
 
   def save
     sql = <<-SQL
